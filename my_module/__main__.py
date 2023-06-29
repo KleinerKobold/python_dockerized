54819591e2e8e21d5077ec4c0a33e6fa68101daa
Markus Köhl <markus.koehl@deutschebahn.com>
2023-06-29 17:29:24 +0200

    chore (refactor): Added docstrings and some refactoring
    Just to make pylint happy ;-)
    
    Signed-off-by: Markus Köhl <markus.koehl@deutschebahn.com>

--- my_module/__main__.py
+++ my_module/__main__.py
@@ -1,24 +1,35 @@
+"""Basic example of a python script running in a docker container
+
+Will print the currect time received from http://worldtimeapi.org
+and some basic information.
+"""
 import os
 
 import requests
 
-def get_current_time():
-    url = "http://worldtimeapi.org/api/timezone/Europe/Berlin" 
 
-    response = requests.get(url)
+def get_current_time() -> str:
+    """Get current time from http://worldtimeapi.org
+
+    Returns:
+        str: string representation of the current time
+    """
+    url = "http://worldtimeapi.org/api/timezone/Europe/Berlin"
+
+    response = requests.get(url=url, timeout=5)
     if response.status_code == 200:
         data = response.json()
         current_time = data["datetime"]
         return current_time
-    else:
-        print("error on request: ", response.status_code)
-        return None
-    
+    print(f'could not get time from {url}: ', response.status_code)
+    return None
 
-def main():
-    print("Python script running")
-    someValue = os.environ.get("someValue")
-    print(f"got someValue from the .env file '{someValue}'")
+def main() -> None:
+    """main function that does all the magic and prints to stdout
+    """
+    print(f'Python script {__name__} is running')
+    some_value = os.environ.get("someValue")
+    print(f'got someValue from the .env file "{some_value}"')
     current_time = get_current_time()
     if current_time:
         print("current time: ", current_time)
